4a5408c48581f0815f9f419eda9cd693746beb7f
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-10 06:06:59 +0530

    aug 9

---
+++ core_python_programs/Module/module_demo_1/Mymath.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    print(f"Addition of {a} and {b} is {a+b}")
+    
+def sub(a,b):
+    print(f"Subtraction of {a} and {b} is {a-b}")
+    
+def mul(a,b):
+    print(f"Multiplication of {a} and {b} is {a*b}")
---
+++ core_python_programs/Module/module_demo_1/math_test.py
@@ -0,0 +1,4 @@
+import Mymath
+
+Mymath.add(10,20)
+Mymath.sub(10,20)
